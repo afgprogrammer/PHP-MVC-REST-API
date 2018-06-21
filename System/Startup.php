1e9eb732fbcc4add9339fea37b22d696f1721028
Mohammad <rto1680@gmail.com>
2018-06-21 11:55:33 +0430

    create startup file for autoload class in system.

---
+++ System/Startup.php
@@ -0,0 +1,13 @@
+<?php
+
+function autoload($class) {
+    $file = SYSTEM . str_replace('\\', '/', $class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    } else {
+        throw new Exception(sprintf('Class { %s } Not Found!', $class));
+    }
+}
+
+spl_autoload_register('autoload');
