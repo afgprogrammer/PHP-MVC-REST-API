41137b40e60cad27ee629277af9c903a5b9f4eec
Mohammad <rto1680@gmail.com>
2018-06-21 09:43:10 +0430

    create index and config file.

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *  Config File For Handel Route, Database And Request
+ * 
+ *  Author: Mohammad Rahmani
+ *  Email: rto1680@gmail.com
+ *  WebPage: mohammadrahmani.com
+ *  
+ */
+
+// Http Url
+$scriptName = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+define('HTTP_URL', '/'. substr_replace(trim($_SERVER['REQUEST_URI'], '/'), '', 0, strlen($scriptName)));
+
+// Define Path Application
+define('SCRIPT', str_replace('\\', '/', rtrim(__DIR__, '/')) . '/');
+define('SYSTEM', SCRIPT . 'System/');
+define('CONTROLLERS', SCRIPT . 'Application/Controllers/');
+define('MODELS', SCRIPT . 'Application/Models/');
+
+// Config Database
+define('DATABASE', [
+    'Port'   => '3307',
+    'Host'   => 'localhost',
+    'Driver' => 'PDO',
+    'Name'   => 'simple-mvc',
+    'User'   => 'root',
+    'Pass'   => '',
+    'Prefix' => 'sm_'
+]);
+
+// DB_PREFIX
+define('DB_PREFIX', 'sm_');
