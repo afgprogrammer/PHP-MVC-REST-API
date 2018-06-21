971030a34b21c4e09f6c05c19cf1e03a5dac42ae
Mohammad <rto1680@gmail.com>
2018-06-21 11:58:05 +0430

    index file change for handle application

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
+require 'config.php';
+require SYSTEM . 'Startup.php';
 
+use Router\Router;
+
+$request = new Http\Request();
+$response = new Http\Response();
+$response->setHeader('Access-Control-Allow-Origin: *');
+$response->setHeader('Content-Type: application/json; charset=UTF-8');
+
+$router = new Router('/' . $request->getUrl(), $request->getMethod());
+require 'Router/Router.php';
+// Rouetr path
+
+// Router Run Request
+$router->run();
+
+// Response Render Content
+$response->render();
