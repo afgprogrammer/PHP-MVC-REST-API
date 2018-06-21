1fa910815b2d610b7d050d25a4b9f2d91027a6b3
Mohammad <rto1680@gmail.com>
2018-06-21 10:30:35 +0430

    create router class for dispatch request URL and route pattern.

---
+++ System/Router/Router.php
@@ -0,0 +1,233 @@
+<?php
+
+/**
+ *
+ * This file is part of simple-mvc-rest-api for PHP.
+ *
+ */
+namespace Router;
+
+class Router {
+
+    /**
+     * route list.
+     * 
+     * @var array
+     */
+    private $router = [];
+
+    /**
+     * match route list.
+     * 
+     * @var array
+     */
+    private $matchRouter = [];
+
+    /**
+     * request url.
+     * 
+     * @var string
+     */
+    private $url;
+
+    /**
+     * request http method.
+     * 
+     * @var string
+     */
+    private $method;
+
+    /**
+     * param list for route pattern
+     * 
+     * @var array
+     */
+    private $params = [];
+
+    /**
+     *  Response Class
+     */
+    private $response;
+
+    /**
+     *  constaruct
+     */
+    public function __construct(string $url, string $method) {
+        $this->url = $url;
+        $this->method = $method;
+
+        // get response class of $GLOBALS var
+        $this->response = $GLOBALS['response'];
+    }
+
+    /**
+     *  set get request http method for route
+     */
+    public function get($pattern, $callback) {
+        $this->addRoute("GET", $pattern, $callback);
+    }
+
+    /**
+     *  set post request http method for route
+     */
+    public function post($pattern, $callback) {
+        $this->addRoute('POST', $pattern, $callback);
+    }
+
+    /**
+     *  set delete request http method for route
+     */
+    public function delete($pattern, $callback) {
+        $this->addRoute('DELETE', $pattern, $callback);
+    }
+
+    /**
+     *  add route object into router var
+     */
+    public function addRoute($method, $pattern, $callback) {
+        array_push($this->router, new Route($method, $pattern, $callback));
+    }
+
+    /**
+     *  filter requests by http method
+     */
+    private function getMatchRoutersByRequestMethod() {
+        foreach ($this->router as $value) {
+            if (strtoupper($this->method) == $value->getMethod())
+                array_push($this->matchRouter, $value);
+        }
+    }
+
+    /**
+     * filter route patterns by url request
+     */
+    private function getMatchRoutersByPattern($pattern) {
+        $this->matchRouter = [];
+        foreach ($pattern as $value) {
+            if ($this->dispatch($this->url, $value->getPattern()))
+                array_push($this->matchRouter, $value);
+        }
+    }
+
+    /**
+     *  dispatch url and pattern
+     */
+    public function dispatch($url, $pattern) {
+        preg_match_all('@:([\w]+)@', $pattern, $params, PREG_PATTERN_ORDER);
+
+        $patternAsRegex = preg_replace_callback('@:([\w]+)@', [$this, 'convertPatternToRegex'], $pattern);
+
+        if (substr($pattern, -1) === '/' ) {
+	        $patternAsRegex = $patternAsRegex . '?';
+	    }
+        $patternAsRegex = '@^' . $patternAsRegex . '$@';
+        
+        // check match request url
+        if (preg_match($patternAsRegex, $url, $paramsValue)) {
+            array_shift($paramsValue);
+            foreach ($params[0] as $key => $value) {
+                $val = substr($value, 1);
+                if ($paramsValue[$val]) {
+                    $this->setParams($val, urlencode($paramsValue[$val]));
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     *  get router
+     */
+    public function getRouter() {
+        return $this->router;
+    }
+
+    /**
+     * set param
+     */
+    private function setParams($key, $value) {
+        $this->params[$key] = $value;
+    }
+
+    /**
+     * Convert Pattern To Regex
+     */
+    private function convertPatternToRegex($matches) {
+        $key = str_replace(':', '', $matches[0]);
+        return '(?P<' . $key . '>[a-zA-Z0-9_\-\.\!\~\*\\\'\(\)\:\@\&\=\$\+,%]+)';
+    }
+
+    protected function trimeRoute($route) {
+        return ltrim($route, '/');
+    }
+
+    protected function trimeUrl($route) {
+        return rtrim($route, '/');
+    }
+
+    /**
+     *  run application
+     */
+    public function run() {
+        if (!is_array($this->router) || empty($this->router)) 
+            throw new Exception('NON-Object Route Set');
+
+        $this->getMatchRoutersByRequestMethod();
+        $this->getMatchRoutersByPattern($this->matchRouter);
+
+        if (!$this->matchRouter || empty($this->matchRouter)) {
+            $this->response->sendStatus(404);
+            $this->response->setContent(['error' => 'Sorry This Route Not Found !', 'status_code' => 404]);
+        } else {
+
+            // cal to callback method
+            if (is_callable($this->matchRouter[0]->getCallback())) {
+                call_user_func($this->matchRouter[0]->getCallback(), $this->params);
+            } else {
+                $this->runController($this->matchRouter[0]->getCallback(), $this->params);
+            }
+        }
+    }
+
+    /**
+     * run as controller
+     */
+    private function runController($controller, $params) {
+        $part = explode('@', $controller);
+        $file = CONTROLLERS . ucfirst($part[0]) . '.php';
+
+        if (file_exists($file)) {
+            require_once($file);
+
+            // controller class
+            $controller = ucfirst($part[0]) . 'Controller';
+
+            if (class_exists($controller)) {
+                $controller = new $controller();
+            } else {
+                $this->response->sendStatus(404);
+                $this->response->setContent(['error' => 'this route not found !', 'status_code' => 404]);
+            }
+
+            // set function in controller
+            if (isset($part[1])) {
+                $method = $part[1];
+                if (!method_exists($controller, $method)) {
+                    $this->response->sendStatus(404);
+                    $this->response->setContent(['error' => 'this route not found !', 'status_code' => 404]);
+                }
+            } else {
+                $method = 'index';
+            }
+
+            // call to controller
+            if (is_callable([$controller, $method]))
+                return call_user_func([$controller, $method], $params);
+            else 
+                $this->response->setContent(['error' => 'this route not found !', 'status_code' => 404]);
+        }
+    }
+}
