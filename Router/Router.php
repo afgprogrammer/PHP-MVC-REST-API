d7c196337287b179eb5a6179aa049d030928f383
Mohammad Rahmani <rto1680@gmail.com>
2019-09-12 19:59:12 +0430

    Add a url with space

--- Router/Router.php
+++ Router/Router.php
@@ -2,6 +2,9 @@
 
 $router->get('/home', 'home@index');
 
+// If you use SPACE in the url, it should convert the space to -, /home-index
+$router->get('/home index', 'home@index');
+
 $router->post('/home', 'home@post');
 
 $router->get('/', function() {
