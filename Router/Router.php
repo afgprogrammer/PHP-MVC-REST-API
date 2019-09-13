55d95272f3d701ddb5fed0b6e211e54bae0817fa
Mohammad Rahmani <rto1680@gmail.com>
2019-09-13 13:43:55 +0430

    Make a upload file route

--- Router/Router.php
+++ Router/Router.php
@@ -5,6 +5,8 @@ $router->get('/home', 'home@index');
 // If you use SPACE in the url, it should convert the space to -, /home-index
 $router->get('/home index', 'home@index');
 
+$router->post('/upload', 'home@uploadImage');
+
 $router->post('/home', 'home@post');
 
 $router->get('/', function() {
