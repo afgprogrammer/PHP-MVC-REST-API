293678b80b7f3b0c1e3b9fdce4d8997215c337d7
Mohammad Rahmani <rto1680@gmail.com>
2019-09-13 13:45:05 +0430

    Make a simple image upload file

--- Application/Controllers/Home.php
+++ Application/Controllers/Home.php
@@ -37,4 +37,41 @@ class ControllersHome extends Controller {
             $this->response->setContent($data);
         }
     }
+
+    public function uploadImage() {
+        if(isset($this->request->files['image'])){
+            $image = $this->request->files['image'];
+            $errors = array();
+
+            // File info
+            $file_name = $image['name'];
+            $file_size = $image['size'];
+            $file_tmp = $image['tmp_name'];
+            $file_type = $image['type'];
+
+            // Get file extension
+            $file_ext = explode('.', $file_name);
+            $file_ext = strtolower(end($file_ext));
+            
+            // White list extensions
+            $extensions = array("jpeg","jpg","png");
+            
+            // Check it's valid file for upload
+            if(in_array($file_ext, $extensions) === false) {
+               $errors[] = "Extension not allowed, please choose a JPEG or PNG file.";
+            }
+            
+            // Check file size
+            if($file_size > 2097152) {
+               $errors[] = 'File size must be exactly 2 MB';
+            }
+            
+            if(empty($errors) == true) {
+               move_uploaded_file($file_tmp, UPLOAD . "Images/" . $file_name);
+               echo "Success";
+            } else {
+               print_r($errors);
+            }
+         }
+    }
 }
