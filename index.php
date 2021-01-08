abc235cd118dc96d1c400930606f883219569a1e
Mohammad Rahmani <rto1680@gmail.com>
2021-01-08 13:49:04 +0430

    Fix route issue

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $response->setHeader("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DEL
 $response->setHeader('Content-Type: application/json; charset=UTF-8');
 
 // set request url and method
-$router = new Router('/' . $request->getUrl(), $request->getMethod());
+$router = new Router($request->getUrl(), $request->getMethod());
 
 // import router file
 require 'Router/Router.php';
